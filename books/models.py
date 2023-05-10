5e77b88c47fbcf1374d2c7089ac4e0716bcdae03
Erfan4708 <abbasy.4708@gmail.com>
2023-05-10 13:46:09 +0330

    Add BookDetailView

--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 
 class Book(models.Model):
@@ -10,4 +10,7 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail' , args= [self.id])
 
