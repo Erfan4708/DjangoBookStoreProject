de68e2dd66aac9658fc232a0ff72cf526d8c38f6
Erfan4708 <abbasy.4708@gmail.com>
2023-05-14 13:55:51 +0330

    add comment to models

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Book(models.Model):
@@ -17,4 +18,20 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book_detail' , args= [self.id])
+
+
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(get_user_model() , on_delete=models.CASCADE)
+    date_time_created = models.DateTimeField(auto_now_add=True)
+    book = models.ForeignKey(Book , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+
+
+
 
