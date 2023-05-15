ead06fb6003240cfa1a161630e268854a7861b8e
Erfan4708 <abbasy.4708@gmail.com>
2023-05-15 13:13:33 +0330

    add comment form to book_detail

--- books/admin.py
+++ books/admin.py
@@ -4,7 +4,7 @@ from .models import Book , Comment
 
 # @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user' , 'book' , 'text' , 'date_time_created' ,)
+    list_display = ('user' , 'book' , 'text' , 'date_time_created' ,'is_active' , 'recommend' ,)
 
 admin.site.register(Book)
 admin.site.register(Comment , CommentAdmin)
